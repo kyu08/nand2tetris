8a59146e14a1e9c89d25b9992a48c473dd8a61da
kyu08 <49891479+kyu08@users.noreply.github.com>
2025-01-29 16:32:45 +0900

    10 wip SubroutineDec#newの実装, テスト

--- 10/src/compiler/ast.rs
+++ 10/src/compiler/ast.rs
@@ -38,7 +38,7 @@ impl Ast {
 struct Class {
     name: ClassName,
     var_dec: Vec<ClassVarDec>,
-    sub_routine_dec: Vec<SubRoutineDec>,
+    subroutine_dec: Vec<SubroutineDec>,
 }
 impl Class {
     // parse結果と次のトークン読み出し位置を返す
@@ -54,10 +54,18 @@ impl Class {
         };
 
         let (var_dec, index) = ClassVarDec::new(tokens, index);
+        let (subroutine_dec, index) = SubroutineDec::new(tokens, index);
+
+        // 最後にセミコロンがあることをチェック
+        match tokens.get(index) {
+            Some(token::Token::Sym(token::Symbol::SemiColon)) => {}
+            _ => panic!("{}", invalid_token(tokens, index)),
+        };
+
         Some(Class {
             name,
             var_dec,
-            sub_routine_dec: vec![],
+            subroutine_dec: vec![],
         })
     }
 }
@@ -155,41 +163,53 @@ enum Type {
     ClassName(String),
 }
 
-struct SubRoutineDec {
-    kind: SubRoutineDecKind,
-    type_: SubRoutineDecType,
-    sub_routine_name: String,
+#[derive(Debug, PartialEq, Eq)]
+struct SubroutineDec {
+    kind: SubroutineDecKind,
+    type_: SubroutineDecType,
+    subroutine_name: String,
     parameter_list: Vec<ParameterList>,
-    body: SubRoutineBody,
+    body: SubroutineBody,
 }
-impl SubRoutineDec {
-    fn new() -> (Vec<Self>, usize) {
+impl SubroutineDec {
+    fn new(tokens: &Vec<token::Token>, index: usize) -> (Vec<Self>, usize) {
+        // TODO: kindの判定
+        // TODO: typeの判定
+        // TODO: nameを取り出す
+        // TODO: parameterList(引数リスト)のパース
+        // TODO: subroutineBodyのパース
         (vec![], 0)
     }
 }
 
-enum SubRoutineDecKind {
+#[derive(Debug, PartialEq, Eq)]
+enum SubroutineDecKind {
     Constructor,
     Function,
     Method,
 }
 
-enum SubRoutineDecType {
+#[derive(Debug, PartialEq, Eq)]
+enum SubroutineDecType {
     Void,
     Type_(Type),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct ParameterList(Vec<(Type, VarName)>);
 
-struct SubRoutineBody {
+#[derive(Debug, PartialEq, Eq)]
+struct SubroutineBody {
     var_dec: Vec<VarDec>,
     statements: Vec<Statement>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct VarDec {
     type_: Type,
     var_name: Vec<VarName>,
 }
+#[derive(Debug, PartialEq, Eq)]
 struct ClassName(token::Identifier);
 impl ClassName {
     fn new(tokens: &Vec<token::Token>, index: usize) -> (Self, usize) {
@@ -201,14 +221,16 @@ impl ClassName {
     }
 }
 
-struct SubRoutineName(token::Identifier);
+struct SubroutineName(token::Identifier);
 #[derive(Debug, PartialEq, Eq)]
 struct VarName(token::Identifier);
 
 /*
  * 文
  */
+#[derive(Debug, PartialEq, Eq)]
 struct Statements(Vec<Statement>);
+#[derive(Debug, PartialEq, Eq)]
 enum Statement {
     Let(LetStatement),
     If(IfStatement),
@@ -217,41 +239,49 @@ enum Statement {
     Return(ReturnStatement),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct LetStatement {
     var_name: VarName,
     // index: Option<Expression>,
     right_hand_side: Expression,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct IfStatement {
     condition: Expression,
     positive_case_body: Statements,
     negative_case_body: Option<Statements>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct WhileStatement {
     condition: Expression,
     body: Statements,
 }
 
-struct DoStatement(SubRoutineCall);
+#[derive(Debug, PartialEq, Eq)]
+struct DoStatement(SubroutineCall);
 
+#[derive(Debug, PartialEq, Eq)]
 struct ReturnStatement(Option<Expression>);
 
 /*
  * 式
  */
+#[derive(Debug, PartialEq, Eq)]
 struct Expression {
     term: Term,
     // TODO: あとでコメントインする
     // op_term: Vec<(Op, Term)>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum Term {
     KeyWordConstant,
     // TODO: あとで拡張する
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum KeyWordConstant {
     True,
     False,
@@ -259,17 +289,20 @@ enum KeyWordConstant {
     This,
 }
 
-struct SubRoutineCall {
+#[derive(Debug, PartialEq, Eq)]
+struct SubroutineCall {
     name: String,
     receiver: Option<Receiver>,
     arguments: Vec<ExpressionList>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum Receiver {
     ClassName(ClassName),
     VarName(VarName),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct ExpressionList(Vec<Expression>);
 
 // TODO: Op
@@ -325,6 +358,84 @@ mod test {
                     ],
                 },
             ],
+            13,
+        );
+        assert_eq!(input, expected);
+    }
+
+    #[test]
+    fn test_subroutine_dec_new() {
+        /*
+        class SquareGame {
+            field Square square;
+            field int direction;
+            constructor SquareGame new() {
+                let square = square;
+                let direction = direction;
+                return square;
+             }
+
+             method void dispose() {
+                do square.dispose();
+                do Memory.deAlloc(square);
+                return;
+             }
+
+             method void moveSquare() {
+                if (direction) { do square.moveUp(); }
+                if (direction) { do square.moveDown(); }
+                if (direction) { do square.moveLeft(); }
+                if (direction) { do square.moveRight(); }
+                do Sys.wait(direction);
+                return;
+             }
+        }
+        */
+        let input = SubroutineDec::new(
+            &vec![
+                token::Token::Key(token::Keyword::Field),
+                token::Token::Identifier(token::Identifier("Square".to_string())),
+                token::Token::Identifier(token::Identifier("square".to_string())),
+                token::Token::Sym(token::Symbol::SemiColon),
+                token::Token::Key(token::Keyword::Field),
+                token::Token::Key(token::Keyword::Int),
+                token::Token::Identifier(token::Identifier("direction".to_string())),
+                token::Token::Sym(token::Symbol::SemiColon),
+                token::Token::Key(token::Keyword::Constructor),
+                token::Token::Identifier(token::Identifier("SquareGame".to_string())),
+                token::Token::Identifier(token::Identifier("new".to_string())),
+                token::Token::Sym(token::Symbol::LeftParen),
+                token::Token::Sym(token::Symbol::RightParen),
+                token::Token::Sym(token::Symbol::LeftBrace),
+                token::Token::Key(token::Keyword::Let),
+                token::Token::Identifier(token::Identifier("square".to_string())),
+                token::Token::Sym(token::Symbol::Equal),
+                token::Token::Identifier(token::Identifier("square".to_string())),
+                token::Token::Sym(token::Symbol::SemiColon),
+                token::Token::Key(token::Keyword::Let),
+                token::Token::Identifier(token::Identifier("direction".to_string())),
+                token::Token::Sym(token::Symbol::Equal),
+                token::Token::Identifier(token::Identifier("direction".to_string())),
+                token::Token::Sym(token::Symbol::SemiColon),
+                token::Token::Key(token::Keyword::Return),
+                token::Token::Identifier(token::Identifier("square".to_string())),
+                token::Token::Sym(token::Symbol::SemiColon),
+                token::Token::Sym(token::Symbol::RightBrace),
+                //  ひとまず1つめの宣言まで
+            ],
+            0,
+        );
+        let expected = (
+            vec![SubroutineDec {
+                kind: SubroutineDecKind::Constructor,
+                type_: SubroutineDecType::Type_(Type::ClassName("SquareGame".to_string())),
+                subroutine_name: "new".to_string(),
+                parameter_list: vec![],
+                body: SubroutineBody {
+                    var_dec: vec![],
+                    statements: vec![],
+                },
+            }],
             13,
         );
         assert_eq!(input, expected);
